abee4e1ed9743fcfdb7d1f0843e92a6d7ad9327f
harish-rzt <harish.v@razorthink.net>
2016-09-14 17:54:28 +0530

    ...

--- src/directives/sidenav/sidenav.directive.js
+++ src/directives/sidenav/sidenav.directive.js
@@ -1,6 +1,57 @@
-angular.module('BookKeeper').directive('bkSidenav', function() {
-    return {
-        templateUrl : "/dist/views/sidenav.html",
-        restrict: "E"
-    };
-});
+angular.module('BookKeeper')
+    .directive('bkSidenav', function () {
+        return {
+            templateUrl: "/dist/views/sidenav.html",
+            restrict: "E",
+            controller: ['$scope', '$route', function ($scope, $route) {
+                $scope.$route = $route;
+
+                $scope.sideNavLinks = [
+                    {
+                        label: "AGGRADE",
+                        sref: "/aggrade"
+                    },
+                    {
+                        label: "BITUMEN",
+                        sref: "/bitumen"
+                    },
+                    {
+                        label: "LDO",
+                        sref: "/ldo"
+                    },
+                    {
+                        label: "DISEAL",
+                        sref: "/diseal"
+                    },
+                    {
+                        label: "OTHER INCOMING OIL",
+                        sref: "/otherIncomingOil"
+                    },
+                    {
+                        label: "POWER READING",
+                        sref: "/powerReading"
+                    },
+                    {
+                        label: "OUTGOING MATERIAL",
+                        sref: "/outgoingMaterial"
+                    },
+                    {
+                        label: "SALARY",
+                        sref: "/salary"
+                    },
+                    {
+                        label: "CASH BOX",
+                        sref: "/cashBox"
+                    },
+                    {
+                        label: "EXPENDITURE",
+                        sref: "/expenditure"
+                    },
+                    {
+                        label: "SUMMARY",
+                        sref: "/Summary"
+                    }
+                ];
+            }]
+        };
+    });
