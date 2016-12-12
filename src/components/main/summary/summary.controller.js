d1d638f8be20164ca6ed86e3194b189922539e45
harish-rzt <harish.v@razorthink.net>
2016-12-12 21:56:44 +0530

    summary page: update bitumen balance

--- src/components/main/summary/summary.controller.js
+++ src/components/main/summary/summary.controller.js
@@ -8,9 +8,11 @@ angular.module('BookKeeper')
         function( $rootScope, $stateParams, $state, Service, $filter){
 
             var self = this;
+
             self.$stateParams = $stateParams;
             self.categories = ["Aggrade", "Bitumen ", "Ldo", "Diesel", "Powerreading", "Incoming material", "Outgoing material", "Salary Statements", "Expenditure"]
-            self.categoriesMap = {"Aggrade": "aggrade",
+            self.categoriesMap = {
+                "Aggrade": "aggrade",
                 "Bitumen ": "bitumen",
                 "Ldo": "ldo",
                 "Diesel": "diesel",
@@ -18,14 +20,38 @@ angular.module('BookKeeper')
                 "Powerreading": "powerreading",
                 "Incoming material": "otherincomingmaterial",
                 "Outgoing material": "outgoingmaterial",
-                "Salary Statements": "salarystatements"}
+                "Salary Statements": "salarystatements"
+            }
             self.getBalances = function(){
+                console.log("refresh")
                 self.toggleBalances && Service.getBalances().then(function(data){
                     self.balances = data;
                 })
             }
             self.reportsModel = {duration: 'monthly'};
-            
+
+            self.bitumenBalancePopup = {
+                visible: false,
+                show: function () {
+                    this.visible = true;
+                    this.model = {};
+                },
+                onUpdate: function (form, value) {
+                    console.log("here", value)
+                    if(form.$valid)
+                        Service.update(_.get(value, 'balance', ''))
+
+                    this.visible = false;
+                    Service.getBalances().then(function(data){
+                        self.balances = data;
+                    })
+                },
+                onClose: function () {
+                    this.visible = false;
+                    this.model = {}
+                }
+            }
+
             self.generateReport= function(){
                 console.log("...",self.reportsModel.duration)
                 var form = {};
@@ -43,7 +69,7 @@ angular.module('BookKeeper')
                 } else {
                     var date = new Date(self.reportsModel.day), y = date.getFullYear(), m = date.getMonth();
                     var firstDay = new Date(y, m, 1);
-                    
+
                     form.startDate = $filter('date')(date, 'dd/MM/yyyy');
                     form.year = $filter('date')(firstDay, 'yyyy');
                     form.month = $filter('date')(firstDay, 'LLLL');
--- src/components/main/summary/summary.service.js
+++ src/components/main/summary/summary.service.js
@@ -10,6 +10,7 @@ angular.module('BookKeeper')
             var self = this;
 
             self.getBalances = function(){
+
                 $rootScope.showLoader = true;
                 return Restangular.one("report/getbalances", sessionStorage.getItem("user"))
                     .get()
@@ -19,6 +20,13 @@ angular.module('BookKeeper')
                         return response.data;
                     })
             };
+
+            self.update = function (value){
+                $rootScope.showLoader = true;
+                return Restangular.one("item/update/bitumen")
+                    .get({value:value, userid: Number(sessionStorage.getItem("user"))})
+                    .then()
+            }
 
             self.generateReport= function(data){
                 $rootScope.showLoader = true;
